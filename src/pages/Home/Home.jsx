f0ca2682716a2a0806739dcaa6b15dc724fe62c6
ysebasst <ysebasst@gmail.com>
2021-12-19 19:43:47 +0000

    Home img added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
-import { HomeStyled } from "./Home.styles";
+import { HomeStyled, ImgStyled } from "./Home.styles";
 
 import MainTitle from "../../components/MainTitle";
 
 export default function Home() {
   return (
     <HomeStyled>
-      <MainTitle title="Home" />
+      <ImgStyled src="/static/novena.jpg" />
     </HomeStyled>
   );
 }
--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -3,3 +3,9 @@ import styled from "styled-components";
 export const HomeStyled = styled.div`
   padding: 1.5rem;
 `;
+
+export const ImgStyled = styled.img`
+  display: block;
+  max-width: 100%;
+  margin: 0 auto;
+`;
